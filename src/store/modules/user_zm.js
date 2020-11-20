4b0443042274e9322ff773715c59ee4eba77710c
zhangmin <572580641@qq.com>
2020-11-20 09:17:56 +0800

    zm

--- src/store/modules/user_zm.js
+++ src/store/modules/user_zm.js
@@ -4,15 +4,17 @@
 
 const state = {
   count:0, //优惠券原始数量
-  // show:0
+  // money:0 //优惠券金额
 }
 
 const getters = {//取state的值，输出值
   show(state){
     return state.count;
-  }
+  },
+  // quan25(state){
+  //   return state.money
+  // }
 }
-
 const actions = {//接收函数
   youhui(ctx){
     ctx.commit('youhui')
@@ -20,6 +22,7 @@ const actions = {//接收函数
   show(ctx,count) {
     ctx.commit("show",count)
   }
+
 }
 
 const mutations = {//改变函数传递数据
@@ -28,7 +31,13 @@ const mutations = {//改变函数传递数据
   },
   show(state,count) {
     state.count = count
-  }
+  },
+  saveNum(state,num){
+    state.count=num
+  },
+  // quan25(state,money) {
+  //   state.money==25
+  // }
 }
 
 export default {
