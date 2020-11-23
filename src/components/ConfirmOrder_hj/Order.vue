4b483d7d5fa4b6fe5f7a8e9c95ac25caa761e4f1
hejun0406 <2786973174@qq.com>
2020-11-23 18:58:46 +0800

    1111

--- src/components/ConfirmOrder_hj/Order.vue
+++ src/components/ConfirmOrder_hj/Order.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="body">
     <div class="header">
-      <img :src="src" width="70px" height="35px">
-      <h2>确认订单</h2>
+      <p>首页 <span>></span> 购物车 <span>></span>确认订单</p>
     </div>
 
     <div class="content1">
       <p>收货地址</p>
-      <div class="box">
-
-
+      <div v-for="(item,index) in form2" class="box" :key="index" @click="changeAddress(index)">
+         <span class="span1">{{item.name}}</span>
+         <span class="span2">{{item.tel}}</span>
+         <p class="p3">{{item.address}}</p>
+         <div class="btn">
+           <button type="text" @click="updateAddress(index)">修改</button>
+           <button type="text" @click="delAddress(index)">删除</button>
+         </div>
       </div>
+      <!--添加收货地址弹框-->
       <div>
-        <el-button type="text" @click="dialogFormVisible = true">+新增收货地址</el-button>
-        <el-dialog title="收货地址" :visible.sync="dialogFormVisible" class="title">
+        <el-button type="text" @click="dialogFormVisible = true" class="addAdress">+新增收货地址</el-button>
+        <el-dialog title="-添加地址-" :visible.sync="dialogFormVisible" class="title">
           <el-form :model="form">
             <el-form-item label="收货人：" :label-width="formLabelWidth">
               <el-input v-model="form.name" autocomplete="off"></el-input>
             </el-form-item>
-            <el-form-item label="手机号码：" :label-width="formLabelWidth" placeholder="请输入11位手机号码">
+            <el-form-item label="手机号码：" :label-width="formLabelWidth">
               <el-input v-model="form.tel" placeholder="请输入11位手机号码"></el-input>
             </el-form-item>
             <el-form-item label="备选号码：" :label-width="formLabelWidth">
@@ -30,7 +35,31 @@
           </el-form>
           <div slot="footer" class="dialog-footer">
             <el-button @click="dialogFormVisible = false" class="btn1">取消</el-button>
-            <el-button type="primary" @click="dialogFormVisible = false" class="btn2">保存并使用</el-button>
+            <el-button type="primary" @click="saveAddress" class="btn2">保存并使用</el-button>
+          </div>
+        </el-dialog>
+      </div>
+
+      <!--修改收货地址的弹框-->
+      <div>
+        <el-dialog title="-修改地址-" :visible.sync="dialogFormVisible1" class="title">
+          <el-form :model="form1">
+            <el-form-item label="收货人：" :label-width="formLabelWidth">
+              <el-input v-model="form1.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="手机号码：" :label-width="formLabelWidth">
+              <el-input v-model="form1.tel" placeholder="请输入11位手机号码"></el-input>
+            </el-form-item>
+            <el-form-item label="备选号码：" :label-width="formLabelWidth">
+              <el-input v-model="form1.tel" placeholder="固定电话或其他手机号码"></el-input>
+            </el-form-item>
+            <el-form-item label="收货地址：" :label-width="formLabelWidth">
+              <el-input v-model="form1.address" placeholder="请输入详细的收货地址"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false" class="btn1">取消</el-button>
+            <el-button type="primary" @click="bcupdataAddress" class="btn2">保存并使用</el-button>
           </div>
         </el-dialog>
       </div>
@@ -39,18 +68,17 @@
     <div class="content2">
       <el-table
         :data="cartProducts"
-        style="width: 1180px"
+        style="width: 1180px;padding-left: 25px"
         width="142">
 <!--        <el-table-column>-->
 <!--          <template slot-scope="scope">-->
 <!--            <img :src="scope.row.src" style="margin-left: 25px">-->
 <!--          </template>-->
 <!--        </el-table-column>-->
-
         <el-table-column
           prop="cinformation_name"
           label="名称"
-          width="500">
+          width="400">
         </el-table-column>
 
         <el-table-column
@@ -62,8 +90,7 @@
         <el-table-column
           prop="cspecifications_commodityprice"
           label="单价"
-          width="200"
-        >
+          width="200">
         </el-table-column>
       </el-table>
     </div>
@@ -78,10 +105,11 @@
       <p class="shipment">商品详细</p>
     </div>
     <div class="content3">
-      <span>应付总额:{{totalPrice}}</span>
-      <div class="Adress">
-        <p>配送至：四川省 成都市 高新区 天府二街 云华路333号</p>
-        <p>张三 123456789</p>
+      <span>应付总额: ￥{{totalPrice}}</span>
+      <div class="Address">
+        <p>收货人：{{form3.name}}</p>
+        <p>收货人电话：{{form3.tel}}</p>
+        <p>收货地址：{{form3.address}}</p>
       </div>
       <button class="btn3" @click="submitOrder">提交订单</button>
     </div>
@@ -96,17 +124,30 @@
     data(){
       return {
         src:src,
-        dialogTableVisible: false,
         dialogFormVisible: false,
+        dialogFormVisible1: false,
+        //修改数据的下标变量
+        myIndex:'',
+        //存入输入的信息，初始为空
         form: {
           name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+          tel:'',
+          address:''
+        },
+        form1:{
+          name: '',
+          tel:'',
+          address:''
+        },
+        //点击保存按钮存入输入的地址信息
+        form2: [
+          { name: 'aaaa',
+            tel:1121212,
+            address: 'adsadasdasdas'
+          }
+        ],
+        form3: {
+
         },
         formLabelWidth: '120px',
          //tableData:[{
@@ -121,18 +162,57 @@
         //     count:1,
         //     price:"￥599.00"
         //   },
-
        // ]
       }
     },
     methods: {
       ...mapActions([]),
+      //添加地址
+      saveAddress(){
+        this.dialogFormVisible=false;
+        // this.form2.name = this.form.name
+        // this.form2.tel = this.form.tel
+        // this.form2.address = this.form.address
+        let s = {name:this.form.name,tel: this.form.tel , address: this.form.address}
+        this.form2.push(s)
+        this.form.name=""
+        this.form.tel=""
+        this.form.address=""
+      },
+      //删除地址
+      delAddress(obj){
+        this.form2.splice(obj,1)
+      },
+      //修改地址
+      bcupdataAddress(){
+        let obj = this.myIndex
+        for (let i = 0 ; i <= this.form2.length ; i++){
+          if ( i == obj ){
+            this.form2[i].name = this.form1.name;
+            this.form2[i].tel = this.form1.tel;
+            this.form2[i].address = this.form1.address;
+          }
+        }
+        this.dialogFormVisible1=false;
+        this.form1.name = ''
+        this.form1.tel = ''
+        this.form1.address = ''
+      },
+      updateAddress(obj){
+        this.dialogFormVisible1=true;
+        this.myIndex = obj
+      },
+      changeAddress(obj){
+        this.form3 = this.form2[obj]
+
+      },
       submitOrder (){
+        //this.$axios.post("http://139.196.200.142:8769/cart/deleteAllCart?uid=1")
         this.$axios.post("http://139.196.200.142:8769/cart/deleteAllCart?uid=1")
         .then(res=>{
-          console.log(res.data)
+          console.log(res.data);
         })
-      }
+      },
     },
     computed: {
       ...mapGetters(['cartProducts','totalPrice',"getConsumerId"]),
@@ -149,20 +229,18 @@
   }
   .header {
     width: 100%;
-    height: 70px;
-    /*border: 1px solid grey;*/
+    height: 40px;
     background-color: white;
   }
-  img {
-    float: left;
-    margin: 0px 20px 0 250px;
+  .header p {
+    margin-left: 260px;
   }
-  h2 {
-    float: left;
+  .header span {
+    color: grey;
   }
   .content1 {
     width: 1180px;
-    height: 200px;
+    /*height: 200px;*/
     margin: 25px auto;
     /*border: 1px solid grey;*/
     background-color: white;
@@ -179,6 +257,12 @@
   }
   .title {
     text-align: center;
+  }
+  .addAdress {
+    width: 250px;
+    height: 120px;
+    text-align: center;
+    color: black;
   }
   .btn1 {
     width: 120px;
@@ -197,11 +281,40 @@
   }
   .box {
     width: 250px;
-    height: 100px;
-    border: 2px dashed grey;
+    height: 120px;
+    border: 1px solid lightgrey;
     float: left;
     margin-left: 30px;
     margin-top: 20px;
+  }
+  .box:hover {
+    border: 1px solid red;
+  }
+  .box:active {
+    border: 2px dashed red;
+  }
+  .box .span1 {
+     font-size: 14px;
+     margin-left: 15px;
+    line-height: 25px;
+  }
+  .box .span2 {
+    font-size: 14px;
+    line-height: 25px;
+    margin-right: 10px;
+  }
+  .box .p3 {
+    font-size: 14px;
+    margin-left: 15px;
+    color: dimgrey;
+  }
+  .box .btn {
+    margin-left: 145px;
+  }
+  .btn button {
+    margin-left: 10px;
+    border: none;
+    outline: none;
   }
   .content2 {
     width:1180px;
@@ -220,7 +333,7 @@
   .section-shipment .title {
     font-size: 16px;
     float: left;
-    margin-left: 25px;
+    margin-left: 35px;
   }
   .section-shipment .shipment {
     float: left;
@@ -239,7 +352,10 @@
     margin: 0 auto;
   }
   .content3 span {
-    margin-left: 950px;
+    margin-left: 990px;
+    color:#ca151e;
+    font-size: 20px;
+    font-weight: 600;
   }
   i {
     color: red;
@@ -249,7 +365,7 @@
   .btn3 {
     width: 120px;
     height: 45px;
-    background-color: #ff0530;
+    background-color: #ca151e;
     color: white;
     border: none;
     outline: none;
@@ -257,12 +373,12 @@
     margin-left: 1030px;
     margin-top: 20px;
   }
-  .Adress {
+  .Address {
     width: 300px;
-    height: 60px;
+    height: 70px;
     border: 1px solid lightgrey;
     background-color: #ffe2ea;
-    font-size: 12px;
+    font-size: 14px;
     margin-left: 848px;
     margin-top: 20px;
   }
